8685630082c275ff61ef4586af97522553b503d6
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2025-01-18 00:48:38 +0200

    fix: increase attempts

--- src/lib/queue.ts
+++ src/lib/queue.ts
@@ -5,7 +5,7 @@ import { redisQueue } from "./redis";
 export const notificationsBulkQueue = new Queue(NOTIFICATIONS_BULK_QUEUE_NAME, {
   connection: redisQueue,
   defaultJobOptions: {
-    attempts: 2,
+    attempts: 3,
     backoff: {
       type: "fixed",
       delay: 30_000,
